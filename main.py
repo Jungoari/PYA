88c72cb4b1ce12c60acdc8de1f14d75de26e8963
ssh9930 <ssh9930@gmail.com>
2019-10-16 15:05:11 +0900

    dfsf

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
+import time
+import alarm_command
 
+__commands__ = ['create', 'delete', 'edit', 'list', 'sleepmode'] #커맨드 모음
+__alarms__ = [] #전체 알람 저장소
+__
+
+
+
+def now():
+	return time.ctime()
 
 def main_shell():
 	while True:
 		in_ = input("pyalarm>")
 
+		for i in commands:
+			if i_ == i:
+
+
+			
 main_shell()
